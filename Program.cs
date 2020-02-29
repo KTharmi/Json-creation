ac6d3af058285b5ed64b0a5b3a379aef149b6a5d
KTharmi <tharmikulendiran5@gmail.com>
2020-02-29 06:34:07 -0800

    initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace JsonCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string JSONresult = JsonConvert.SerializeObject(emp);
+            string path = @"D:\projectTesting\employee.json";
+
+            using (var tw = new StreamWriter(path, true))
+            {
+                tw.WriteLine(JSONresult.ToString());
+                tw.Close();
+            }
+        }
+    }
+}
